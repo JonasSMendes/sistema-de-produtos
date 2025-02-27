7cf2bd383a4dc1ed58bc40e97953cc51b5e606d3
jonas mendes <jonas.jsm2903@gmail.com>
2025-02-27 07:15:34 -0300

    Classe produto

---
+++ src/main/java/com/JonasSMendes/produtosapi/entity/Produto.java
@@ -0,0 +1,23 @@
+package com.JonasSMendes.produtosapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+}
